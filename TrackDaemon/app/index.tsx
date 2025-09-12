fe789d8babdd1bcc10dd394da3f56dabf7dad8fb
helenh23 <Lenn_Elias.Heinkel@Student.Reutlingen-University.de>
2025-09-12 20:13:00 +0200

    App reset durchgeführt und angepasst sodass Änderungen auch auf dem Handy angezeigt werden

---
+++ TrackDaemon/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>TrackDaemon</Text>
+      <Text>test</Text>
+    </View>
+  );
+}
